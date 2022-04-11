d88caacea109e51ccb179813121a88714ad7a7c6
l1ttps <l1ttps443@gmail.com>
2022-04-11 11:56:42 +0700

    feat: generate unique id for browser

---
+++ src/utils/fingerPrint.ts
@@ -0,0 +1,13 @@
+import hash from "murmurhash-js/murmurhash2_gc";
+import sha256 from "crypto-js/sha256";
+import getBrowserData from "./getBrowserInfo";
+import cryptoJs from "crypto-js";
+
+const getDeviceId = () =>
+  sha256(hash(getBrowserData(), 256).toString()).toString(cryptoJs.enc.Hex);
+
+const genKeyStorage = () => {
+  return getDeviceId().slice(0, 6);
+};
+
+export { getDeviceId, genKeyStorage };
