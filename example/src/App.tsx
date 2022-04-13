fe631663a2dd394fe9919399b341a96ee71d728f
l1ttps <l1ttps443@gmail.com>
2022-04-13 05:46:03 +0000

    feat: example getMultiple

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -14,7 +14,7 @@ const App = () => {
 
   const handleSetNewValue = (event: any) => {
     event.preventDefault();
-    if ((newValue.key !== '', newValue.value !== '')) {
+    if ((newValue.key !== '' || newValue.value !== '')) {
       storage.setItem(newValue.key, newValue.value)
       setNewValue(initialValue)
     }
@@ -76,7 +76,11 @@ const App = () => {
               <div className="form-check">
                 <input checked={!getAll} onChange={() => setGetAll(false)} className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckChecked" />
                 <label className="form-check-label inline-block text-gray-800" htmlFor="flexCheckChecked">
-                  <pre>storage.getMultiple()</pre>
+                  <pre>storage.getMultiple([{Object.keys(storage.getAll()|| {}).map((key, index) => {
+                    return index <= 3 && <span key={index}>
+                      "{key}"{index < Object.keys(storage.getAll()).length - 1 && ","}
+                    </span>
+                  })}])</pre>
                 </label>
               </div>
             </div>
